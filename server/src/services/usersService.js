63240091dc6ba0c3aee5d523119ab618b791940e
Kevin Huang <kevhuang@chapman.edu>
2024-04-13 22:24:53 -0700

    commit

---
+++ server/src/services/usersService.js
@@ -0,0 +1,14 @@
+const asyncHandler = require('express-async-handler');
+const dbHandler = require("../database/dbHandler.js");
+const { ObjectId } = require('mongodb'); // Import ObjectId from MongoDB library
+
+const getUserByID = async(userID) =>{
+    console.log(userID);
+    const objectID = new ObjectId(userID); 
+    return dbHandler.readDocument('users', '_id', objectID); //collectionName, fieldName, fieldValue
+
+};
+
+module.exports = {
+    getUserByID
+}
